70242271314aea4271db88ea1b8f10d8ece28be2
marcello <mcfanda@gmail.com>
2021-04-27 19:22:45 +0200

    setup

---
+++ R/constants.R
@@ -0,0 +1,17 @@
+# folders should be absolute or relative to the docssource folder
+# do not use the trailing slash
+MODULE_FOLDER="/home/marcello/Skinner/Forge/jamovi/pathj/pathj/"
+SOURCE_FOLDER="pubs"
+TARGET_FOLDER="../"
+DATALINK<-"https://raw.githubusercontent.com/mcfanda/gamlj_docs/master/data"
+
+# These handle the release notes from commits
+MODULE_REPO="pathj"
+MODULE_REPO_OWNER="pathj"
+FIRST_VERSION=NULL
+BANNED_COMMITS=list("initialize","fix commits","remove some marks","typo in random table #73","1.5.0")
+BANNED_COMMITS_GREP=list("^#","^!","^Merge branch","spelling")
+
+# These handle the R vignettes
+
+VIGNETTES_FOLDER="/home/marcello/Skinner/Forge/jamovi/pathj/pathj/vignettes"
