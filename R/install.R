7d79e0e2c3637075bf822bb6e1bbc0e38be42b55
marcello <mcfanda@gmail.com>
2021-04-27 19:41:59 +0200

    updates

--- R/install.R
+++ R/install.R
@@ -5,17 +5,12 @@
   #copy_vignettes()
   msg<-"updates"
   render_site("docssource/",output_format = "html_document")
-  system("git add .")
-  system(paste('git commit -m "',msg,'"'))
-  system("git push origin master")
-  
   ### the following is needed for organization pages
-  source("R/constants.R")
   cmd<-paste("cp -R ",paste0(SOURCE_FOLDER,"/*"),paste0(TARGET_FOLDER,"/"))
   system(cmd)
   setwd(TARGET_FOLDER)
   system("git add .")
   system(paste('git commit -m "',msg,'"'))
-  system("git push origin master")
+  system("git push ")
   setwd(HERE)
   
