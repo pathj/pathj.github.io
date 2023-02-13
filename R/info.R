53931c5502c0fb30d2ca69ec431a3674b009a86c
mcfanda <mcfanda@gmail.com>
2023-02-13 15:34:20 +0100

    updates

---
+++ R/info.R
@@ -0,0 +1,23 @@
+# folders should be absolute or relative to the docssource folder
+# do not use the trailing slash
+
+PROJECT_FOLDER="~/Skinner/Forge/jamovi/pathj/pathj.github.io"
+
+MODULE_NAME="PATHj"
+MODULE_FOLDER="~/Skinner/Forge/jamovi/pathj/pathj/"
+PROJECT_FOLDER="~/Skinner/Forge/jamovi/pathj/pathj.github.io"
+SOURCE_FOLDER="docssource"
+#DATALINK<-"https://raw.githubusercontent.com/mcfanda/gamlj_docs/master/data"
+
+# These handle the release notes from commits
+MODULE_REPO="pathj"
+MODULE_REPO_OWNER="pathj"
+MODULE_LINK="https://github.com/pathj/pathj/"
+MODULE_EMAIL="mcfanda@gmail.com"
+FIRST_VERSION="Version.0.0.1"
+BANNED_COMMITS=list("initialize","fix commits","remove some marks","typo in random table #73","1.5.0")
+BANNED_COMMITS_GREP=list("^#","^!","^Merge branch","spelling")
+
+# These handle the R vignettes
+
+VIGNETTES_FOLDER="~/Skinner/Forge/jamovi/pathj/pathj/vignettes"
